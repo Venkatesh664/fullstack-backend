2ce44628b2247f3088f6bf2866366e3d9e37df87
Venkatesh664 <103093498+Venkatesh664@users.noreply.github.com>
2025-05-28 14:41:58 +0530

    Update FullstackBackendApplication.java

--- src/main/java/com/codewitharjun/fullstackbackend/FullstackBackendApplication.java
+++ src/main/java/com/codewitharjun/fullstackbackend/FullstackBackendApplication.java
@@ -1,11 +1,19 @@
+package com.codewitharjun.fullstackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
 public class FullstackBackendApplication extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(FullstackBackendApplication.class, args);
     }
 
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(FullstackBackendApplication.class);
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FullstackBackendApplication.class);
     }
 }
